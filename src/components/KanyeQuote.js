45e33f131c70844aa007d7508d398854171b271f
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-07-09 11:58:16 +0800

    added functionality to retrieve new quote

--- src/components/KanyeQuote.js
+++ src/components/KanyeQuote.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import getKanyeQuote from '../api/getKanyeQuote'
 
 class KanyeQuote extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentQuote: '',
+      favorites: []
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.updateCurrentQuote();
+  }
+
+  componentDidMount() {
+    this.updateCurrentQuote();
+  }
+
+  updateCurrentQuote() {
+    getKanyeQuote().then((quote) => {
+      this.setState({currentQuote: quote.quote})
+    })
+  }
+
   render() {
     return (
       <div className="kanyequote" >
         <h1>Kanye-West Quote</h1>
-        <p>Test quote</p>
-        <button>Get Quote</button>
+        <p>{this.state.currentQuote}</p>
+        <button onClick={this.handleClick} >Get Quote</button>
         <button>Add</button>
       </div>
     )
